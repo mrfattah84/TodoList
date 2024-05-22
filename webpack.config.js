30e5dc8b0280324ed3ea7f5adb74d2914d3a4346
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-05-22 15:04:07 +0330

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { title } = require('process');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: 'images/[hash][ext][query]',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do List',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
+}
